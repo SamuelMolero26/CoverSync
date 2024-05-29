478f5eee71842dead1ec6ad37210b7bf1d4f53b0
Andres Wearden <andreswearde@utexas.edu>
2024-05-28 20:27:20 -0500

    starter code

---
+++ ProjectCoverSync/src/main/java/org/login/LoginController.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+}
---
+++ ProjectCoverSync/src/main/java/org/login/LoginGUI.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../resources/loginPage.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("My Application");
+        primaryStage.show();
+    }
+}
