d7131d3d6dcd286a5df7c25037bcf4e963032a32
Samuel Molero <samuel@pop-os.lan>
2024-05-29 22:29:13 -0500

    new database changes

---
+++ ProjectCoverSync/target/classes/org/database/Customer.java
@@ -0,0 +1,42 @@
+package org;
+
+
+public class Customer {
+    private int  id;
+    private String name;
+    private String email;
+    private String SSN;
+    private String number;
+
+    //for login
+    
+
+    //getters
+    public int getID() { return id;}
+
+    public String getName() {return name;}
+
+    public String getEmail() {return email;}
+
+    public String getSSN() { return SSN;}
+
+    public String getNumber() { return number;}
+
+    //setters
+
+    public void setId(int id) { this.id = id;}
+
+    public void setName(String name) {this.name = name; }
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setSSN(String SSN) {this.SSN = SSN; }
+
+    public void setNumber(String number) {this.number = number;}
+
+
+
+
+}
+
+
