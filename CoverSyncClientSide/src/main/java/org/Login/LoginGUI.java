c5c1100ae88a980435f0a3da6f94a0f3a5c9d9af
DiegoWearden <diegowearden@utexas.edu>
2024-05-30 00:05:09 -0500

    username and password gets sent to server

--- CoverSyncClientSide/src/main/java/org/Login/LoginGUI.java
+++ CoverSyncClientSide/src/main/java/org/Login/LoginGUI.java
@@ -1,16 +1,21 @@
-package org.login;
+package org.Login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.Client.Client;
 
 public class LoginGUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Client client = new Client();
+        client.setUpNetworking();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/loginPage.fxml"));
         Parent root = loader.load();
+        LoginController controller = loader.getController();
+        controller.setClient(client);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("My Application");
