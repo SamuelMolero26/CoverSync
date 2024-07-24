cf03941befbd73134ab30650c68b188130cd5d22
Samuel jose Molero gonzalez <samuel@Samuels-MBP.lan>
2024-07-23 19:13:26 -0500

    First Stages - mainly front-end

---
+++ coversync/src/LoginPage.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+
+import './App.css';
+
+import 
+
+function LoginPage() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.taget.value);
+    };
+
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //console.log('Login with:', username, password);
+
+    };
+
+    return (
+        <div className = 'login-page'>
+            <form onSubmit = {handleSubmit} className = "login-form">
+                <div>
+                    <label>Username: </label>
+                    <input type="text" value={username} onChange={handleUsernameChange} />  </div>
+                <div>
+                    <label>Password</label>
+                    <input type = "password" value={password} onChange ={handlePasswordChange} />
+                </div>
+                <button type ="submit"> login</button>
+            </form>
+        </div>
+    )
+
+}
+export default LoginPage;
